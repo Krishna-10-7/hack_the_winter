8d7201e746573673564edc890a88b6cd87233c8e
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-11-01 19:56:52 +0530

    change to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,18 @@ import react from "@astrojs/react";
 import swup from "@swup/astro";
 import fonts from "./fonts";
 
+import mdx from "@astrojs/mdx";
+
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hackthewinter.dev",
-	integrations: [react()],
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	experimental: {
-		fonts,
-	},
+    site: "https://hackthewinter.dev",
+    integrations: [react(), mdx(), lenis()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    experimental: {
+        fonts,
+    },
 });
