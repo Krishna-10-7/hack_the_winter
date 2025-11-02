6c54f256198a689ae2d48688a021880ef9c934bf
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2025-11-02 21:01:41 +0530

    remove unused

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-import swup from "@swup/astro";
 import fonts from "./fonts";
 
 import mdx from "@astrojs/mdx";
